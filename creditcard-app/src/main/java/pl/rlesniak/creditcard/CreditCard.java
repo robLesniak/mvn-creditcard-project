b20e132bb364698114099d75ec03a00df79b5a09
Michal Nowakowski <michal951now@gmail.com>
2018-11-07 21:32:41 +0100

    added void repay() method

--- creditcard-app/src/main/java/pl/rlesniak/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/rlesniak/creditcard/CreditCard.java
@@ -1,11 +1,39 @@
 package pl.rlesniak.creditcard;
 
 class CreditCard {
-    public void assignLimit(double money) {
-        
+private boolean isBlocked;
+
+    private double balance;
+    private double cardLimit;
+
+    
+    public void assignLimit(double limit){
+        balance = limit;
+        cardLimit = limit;
     }
     
-    public double getLimit() {
+    public double getLimit(){
         return 2000;
     }
+    
+
+    public void block(){
+        isBlocked = true;  
+    }
+    
+    public boolean isBlocked(){
+        return isBlocked;
+
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+    
+    public double getBalance() {
+        return balance;
+
+	}
+    public void repay repayDebt(){
+		//didn't remeber what this method should do :(
+	}
 }
